c20bc13b49c41e3db2cef2c98de1bb74ae360159
Miguel Angel Cardona <miguel.acardona98@gmail.com>
2025-11-06 23:15:22 -0500

    Async Documentation and Eureka enruting

--- src/main/java/com/arka/notifications/NotificationsApplication.java
+++ src/main/java/com/arka/notifications/NotificationsApplication.java
@@ -2,8 +2,10 @@ package com.arka.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationsApplication {
 
 	public static void main(String[] args) {
